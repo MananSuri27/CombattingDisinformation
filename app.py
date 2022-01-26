3aee1bcef7806c992c66669e086a59906f627a1f
Manan Suri <manansuri27@gmail.com>
2022-01-27 03:26:35 +0530

    added predict route to flask server

---
+++ app.py
@@ -0,0 +1,68 @@
+from asyncio.windows_events import NULL
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import requests
+from flask_cors import CORS
+from newspaper import Article
+
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+classifier = joblib.load('./model/pipeline.pkl')
+newsitesdata=pd.read_csv('./datasets/newsites.tsv', sep='\s+', header=0 ) 
+# .\venv\Scripts\activate
+
+# make predict route a post route
+# add route to fetch newsmedia ranking
+# add route to get related news articles
+
+@app.route("/<name>")
+def home(name):
+     return f'{name}'
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    json_ = request.get_json()
+
+    domain = urlparse(json_['url']).netloc
+    domain_normalised = '.'.join(domain.split('.')[-2:])
+    fact = newsitesdata.loc[newsitesdata['source_url_normalized']== domain_normalised]['fact'].values[0] if newsitesdata.loc[newsitesdata['source_url_normalized']== domain_normalised].empty==False else 0
+    bias = newsitesdata.loc[newsitesdata['source_url_normalized']== domain_normalised]['bias'].values[0] if newsitesdata.loc[newsitesdata['source_url_normalized']== domain_normalised].empty==False else NULL
+
+    summary=0
+
+
+ 
+
+    if 'text' in json_.keys():       
+        pred= classifier.predict_proba([json_['text']])
+    else:
+        req = requests.get(json_['url'])
+        text = BeautifulSoup(req.text, "html.parser").title.string.split("|")[0]
+        print(text)
+        pred = classifier.predict_proba([text])
+
+        article=Article(json_['url'])
+        article.download()
+        article.parse()
+        article.nlp()
+
+        summary = article.summary
+
+    return jsonify({'pred': pred[0][1], 'fact':fact, 'bias':bias, 'summary':summary})
+
+    
+
+if __name__ == "__main__":
+    classifier = joblib.load('./model/pipeline.pkl')
+    app.run(debug=True)
+
