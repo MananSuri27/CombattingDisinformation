3ede91b885d164a9c78822e69978aa76dfa27888
Arsh Kohli <kohliarsh9@gmail.com>
2022-01-27 02:51:18 +0530

    add circular bar ui

--- extension/src/MainPrediction/MainPrediction.js
+++ extension/src/MainPrediction/MainPrediction.js
@@ -1,12 +1,31 @@
 import './mainPred.css';
 import { Button, Grid, Paper } from '@mui/material';
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
 
-export const MainPrediction = ({bias, fact, pred}) => {
-    return (
-        <Grid item container justifyContent={'center'}>
-        <Paper elevation={2} className="predictionContainer">
-        </Paper>
-    </Grid>
 
-    )
-}
+export const MainPrediction = ({ bias, fact, pred }) => {
+    const circleColour = "green";
+	return (
+		<Grid item container justifyContent={'center'}>
+			<Paper elevation={2} className="predictionContainer">
+            <Grid container xs={12} alignItems={"center"} justifyContent={"center"}>
+					<Grid container item xs={6} style={{ padding: '3em' }}>
+                        <div>
+						<CircularProgressbarWithChildren
+							value={pred}
+							strokeWidth={5}
+							styles={buildStyles({
+								textColor: 'black',
+								pathColor: circleColour
+							})}
+						>
+							<div style={{ fontSize: '29px', lineHeight: '22px' }}>{pred}%</div>
+							<div style={{ fontSize: '10px', fontWeight: '500' }}>RELIABILITY</div>
+						</CircularProgressbarWithChildren>
+                        </div>
+					</Grid>
+                </Grid>
+			</Paper>
+		</Grid>
+	);
+};
