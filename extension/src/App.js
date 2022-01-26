0f3029907b83c80065bb1fd4ea67acf4fa9cdb27
Arsh Kohli <kohliarsh9@gmail.com>
2022-01-27 02:31:57 +0530

    remove boilerplate code

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { removeStopwords } from 'stopword';
 
 function App() {
+  const [ url, setUrl ] = useState('');
+	const [ title, setTitle ] = useState('');
+	const [ strap, setStrap ] = useState('');
+	const [ predictionData, setPredictionData ] = useState({});
+	const [ loading, setLoading ] = useState(true);
+	const [ newsLoading, setNewsLoading ] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
