7bff0b614f123fb227e50dc309387bb3ca1056e5
Arsh Kohli <kohliarsh9@gmail.com>
2022-01-27 02:40:19 +0530

    get url from tab

--- extension/src/App.js
+++ extension/src/App.js
@@ -4,17 +4,22 @@ import { removeStopwords } from 'stopword';
 import { Logo } from './Logo/Logo';
 
 function App() {
-  const [ url, setUrl ] = useState('');
+	const [ url, setUrl ] = useState('');
 	const [ title, setTitle ] = useState('');
 	const [ strap, setStrap ] = useState('');
 	const [ predictionData, setPredictionData ] = useState({});
 	const [ loading, setLoading ] = useState(true);
 	const [ newsLoading, setNewsLoading ] = useState(true);
-  return (
-    <div>
-      <Logo/>
-    </div>
-  );
+
+	useEffect(() => {
+		const queryInfo = { active: true, lastFocusedWindow: true };
+
+	}, []);
+	return (
+		<div>
+			<Logo />
+		</div>
+	);
 }
 
 export default App;
